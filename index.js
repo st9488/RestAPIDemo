0c366468c8b125bffd47151abd16fc5317d4c1f6
Shamitha Thumma <sthumma6104@gmail.com>
2022-11-11 20:14:50 -0600

    comments

---
+++ index.js
@@ -0,0 +1,13 @@
+// STEP 1: import express
+
+// STEP 2: parse the JSON data using express
+
+// STEP 3: add a movie to the list
+
+// STEP 4: search for a movie in the list
+
+// STEP 5: update a movie in the list
+
+// STEP 6: remove a movie from the list
+
+// set the server to listen at port
